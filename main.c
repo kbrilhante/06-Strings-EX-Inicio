9d41336038af7965bd9a6c5fdba97d8520255028
Karina <kbrilhante@gmail.com>
2024-09-03 23:01:44 -0300

    Initial commit

---
+++ main.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#define ex00
+
+/*** EXERCICIOS STRINGS ***/
+
+#ifdef ex01
+/*
+	DESAFIO: Monta Frases
+	Escreva um programa que recebe 5 palavras e retorna essas palavras em formato
+	de frase. Por exemplo:
+	O
+	rato
+	roeu
+	a
+	roupa
+	> O rato roeu a roupa
+*/
+int main(int argc, char **argv)
+{
+	printf("");
+    getchar();
+	return 0;
+}
+#endif
+
+#ifdef ex02
+/*
+	DESAFIO: Troca um texto para maiusculas
+	Dica: a biblioteca ctype.h tem o comando toupper(c) que muda um char para maiúscula
+*/
+int main(int argc, char **argv)
+{
+	printf("");
+    getchar();
+	return 0;
+}
+#endif
+
+#ifdef ex03
+/*
+	DESAFIO: Palíndromo
+	Um palídromo é uma palavra é igual quando é lida da direita para a esquerda.
+	exemplo:
+	ARARA
+	RADAR
+	OVO
+	OSSO
+*/
+int main(int argc, char **argv)
+{
+	printf("");
+    getchar();
+	return 0;
+}
+#endif
+
+#ifdef ex04
+/*
+	DESAFIO: Senha
+	Crie um programa que pede a senha ate que ela seja digitada
+*/
+int main(int argc, char **argv)
+{
+	printf("");
+    getchar();
+	return 0;
+}
+#endif
